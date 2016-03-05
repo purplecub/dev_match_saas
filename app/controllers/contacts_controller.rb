ed7e420363f372cd3f1f679b77df9586439aab79
purplecub <purplecub1@icloud.com>
2016-03-05 22:33:10 +0000

    Corrected errors with contact form and sending

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,14 +1,15 @@
 class ContactsController < ApplicationController
     
     def new
-        @contact = Contact.new(contact_params)
+        @contact = Contact.new
     end
     
     def create
+        @contact = Contact.new(contact_params)
         if @contact.save
             name = params[:contact][:name]
             email = params[:contact][:email]
-            body = params[:contact][comments]
+            body = params[:contact][:comments]
             ContactMailer.contact_email(name, email, body).deliver
             
             flash[:success] = 'Message sent.'
