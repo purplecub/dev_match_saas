f1db19dcdf6ed009eaf216dde1d4bb24ceda4bf7
purplecub <purplecub1@icloud.com>
2016-03-05 22:07:52 +0000

    Added flash messages to contact form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,10 +1,22 @@
 class ContactsController < ApplicationController
     
     def new
-        @contact = Contact.new
+        @contact = Contact.new(contact_params)
     end
     
     def create
+        if @contact.save
+            flash[:success] = 'Message sent.'
+            redirect_to new_contact_path
+        else
+            flash[:error] = 'An error occurred. Message not sent.'
+            redirect_to new_contact_path
+        end
+    end
+    
+    private
+    def contact_params
+        params.require(:contact).permit(:name, :email, :comments)
     end
     
 end
