5c63d783785391df76da4d339af8a669bf493c89
purplecub <purplecub1@icloud.com>
2016-03-05 22:18:46 +0000

    Set up contact email and method to deliver message

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,6 +6,11 @@ class ContactsController < ApplicationController
     
     def create
         if @contact.save
+            name = params[:contact][:name]
+            email = params[:contact][:email]
+            body = params[:contact][comments]
+            ContactMailer.contact_email(name, email, body).deliver
+            
             flash[:success] = 'Message sent.'
             redirect_to new_contact_path
         else
