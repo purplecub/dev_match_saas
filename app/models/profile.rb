2acf75854812c2e6ccb7a6f24ca93572f043d74c
purplecub <purplecub1@icloud.com>
2016-03-25 00:10:00 +0000

    Created profile form and added validations

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,6 @@
 class Profile < ActiveRecord::Base
     belongs_to :user
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :job_title, presence: true
 end
